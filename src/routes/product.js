c7955c65e97a44b8810af731ba0cb1d70ec3e545
Deepak Chawla <deepakchawla078@gmail.com>
2021-12-06 14:54:44 +0500

    updated

--- src/routes/product.js
+++ src/routes/product.js
@@ -9,8 +9,8 @@ const path = require('path');
 router.post('/product/add', requireSignIn, upload.array('productPicture'), addProduct);
 router.get("/product/getSellerProducts",requireSignIn, getSellerProducts);
 router.delete("/product/deleteProductById", requireSignIn, deleteProductById);
-router.get("/products/:slug", getProductsBySlug);
 router.get("/product/getProducts", getProducts);
+router.get("/products/:slug", getProductsBySlug);
 router.get("/product/:productId", getProductDetailsById);
 router.get("/product/:search", searchProduct);
 
